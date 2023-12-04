3cf9dae750f84b05e6ca62eeb81322a9eb4c6873
Marco Rosinus Serrano <marcorosinusserrano@gmail.com>
2023-12-04 17:46:01 +0100

    fixed menu banner links

--- app/(weimar)/components/menu/MenuBanner.tsx
+++ app/(weimar)/components/menu/MenuBanner.tsx
@@ -4,12 +4,12 @@ import { MenuBannerItem } from "./MenuItemBanner";
 export function MenuBanner() {
 	return (
 		<center className="bg-light-pink text-olive sticky top-0 z-10 text-h1resp">
-			<MenuBannerItem href="/weimar/about">About</MenuBannerItem>,{" "}
-			<MenuBannerItem href="/weimar/program">Programm</MenuBannerItem>,{" "}
-			<MenuBannerItem href="/weimar/awareness">Awareness</MenuBannerItem>,{" "}
-			<MenuBannerItem href="/weimar">Kontakt</MenuBannerItem>{" "}
+			<MenuBannerItem href="/about">About</MenuBannerItem>,{" "}
+			<MenuBannerItem href="/program">Programm</MenuBannerItem>,{" "}
+			<MenuBannerItem href="/awareness">Awareness</MenuBannerItem>,{" "}
+			<MenuBannerItem href="/">Kontakt</MenuBannerItem>{" "}
 			<FirstLetterWonder>&</FirstLetterWonder>{" "}
-			<MenuBannerItem href="/weimar/imprint">Impressum</MenuBannerItem>
+			<MenuBannerItem href="/imprint">Impressum</MenuBannerItem>
 		</center>
 	);
 }
